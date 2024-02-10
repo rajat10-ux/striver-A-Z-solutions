6147c65a4d6e2a16a1889ccad138179f1b7ec6d9
Rajat Kumar <103815726+rajat10-ux@users.noreply.github.com>
2024-02-10 16:40:50 +0530

    Create check_array_sorted_or_rotated.cpp

---
+++ array/check_array_sorted_or_rotated.cpp
@@ -0,0 +1,13 @@
+bool check(vector<int>& nums) {
+    int count=0;
+for(int i=0;i<nums.size();i++){
+// if(nums[i+1]==nums[i])continue;
+if(nums[i]>nums[(i+1)%nums.size()]){
+count++;
+}
+}
+if(count>1){
+return false;
+}
+return true;   
+}
