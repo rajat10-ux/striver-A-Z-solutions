d56ec5179937108acfd4e8f51c4950b73f527ce8
Rajat Kumar <103815726+rajat10-ux@users.noreply.github.com>
2024-02-10 16:58:09 +0530

    Linearsearch.cpp

---
+++ array/Linearsearch.cpp
@@ -0,0 +1,12 @@
+int linearSearch(int n, int num, vector<int> &arr)
+{
+    // Write your code here.
+    for(int i=0;i<n;i++){
+        if(arr[i]==num){
+            return i;
+        }
+    }
+    return -1;
+}
+// TC O(N)
+//SC O(1)
