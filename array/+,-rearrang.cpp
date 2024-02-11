405d339a06a4e7ebc2f09b79945bbe1009a73d25
Rajat Kumar <103815726+rajat10-ux@users.noreply.github.com>
2024-02-11 21:50:29 +0530

    Create +,-rearrang.cpp

---
+++ array/+,-rearrang.cpp
@@ -0,0 +1,18 @@
+vector<int> alternateNumbers(vector<int>&arr) {
+    // Write your code here.
+    int i,n=arr.size();
+    vector<int>ans(n,0);
+   int pos=0,neg=1;
+   for(i=0;i<n;i++){
+       if(arr[i]<0){
+           ans[neg]=arr[i];
+           neg+=2;
+       }
+       else{
+             ans[pos]=arr[i];
+           pos+=2;
+       }
+   }
+   return ans;
+}
+//TC O(N) SC O(N)
