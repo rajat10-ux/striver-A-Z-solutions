909e65535fb0889cb0b60cb71ae9314160868ddc
Rajat Kumar <103815726+rajat10-ux@users.noreply.github.com>
2024-02-16 23:21:50 +0530

    Create sqaureroot.cpp

---
+++ Binary_Search/sqaureroot.cpp
@@ -0,0 +1,17 @@
+int floorSqrt(int n)
+{
+    int low = 1, high = n;
+    while (low <= high) {
+        long long mid = (low + high) / 2;
+        long long val = mid * mid;
+        if (val <= (long long)(n)) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return high;
+}
+
+//TC O(NLOGN)
