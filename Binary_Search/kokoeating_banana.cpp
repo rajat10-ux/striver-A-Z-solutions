781158af05d49185cf212d2643fffdc9bdc2ab35
Rajat Kumar <103815726+rajat10-ux@users.noreply.github.com>
2024-02-17 11:48:59 +0530

    Create kokoeating_banana.cpp

---
+++ Binary_Search/kokoeating_banana.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+int findMax(vector<int> &v) {
+    int maxi = INT_MIN;
+    int n = v.size();
+    //find the maximum:
+    for (int i = 0; i < n; i++) {
+        maxi = max(maxi, v[i]);
+    }
+    return maxi;
+}
+long long calculateTotalHours(vector<int> &v, int hourly) {
+    long long  totalH = 0;
+    int n = v.size();
+    for (int i = 0; i < n; i++) {
+        totalH += ceil((double)(v[i]) / (double)(hourly));
+    }
+    return totalH;
+}
+
+int minimumRateToEatBananas(vector<int> v, int h) {
+    int low = 1, high = findMax(v);
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        long long totalH = calculateTotalHours(v, mid);
+        if (totalH <= h) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return low;
+}
+//TC O(NLOGN)
