1ac6908d68d5b743d7e6387e4224ccf129420f12
Rajat Kumar <103815726+rajat10-ux@users.noreply.github.com>
2024-02-21 21:14:05 +0530

    Update rowwithmax1.cpp

--- Binary_Search/rowwithmax1.cpp
+++ Binary_Search/rowwithmax1.cpp
@@ -1,61 +1,24 @@
-int lowerindex(vector<int> &arr,int n,int x){
-
- int low=0;
-
- int high=n-1;
-
- int ans=n;
-
- while(low<=high){
-
-     int mid=(low+high)/2;
-
-     if(arr[mid]>=x){
-
-         high=mid-1;
-
-         ans=mid;
-
-     }
-
-     else{
-
-         low=mid+1;
-
-     }
-
- }
-
- return ans;
-
-}
-
- 
-
-int rowWithMax1s(vector<vector<int>> &matrix, int n, int m) {
-
-    int result=-1;
-
- 
-
-    int rowIndex=-1;
-
-    for (int i = 0; i < n; i++) {
-
-        int countOnes= m- lowerindex(matrix[i],m,1);
-
-        if(countOnes >result){
-
-            result=countOnes;
-
-            rowIndex=i;
-
+int upperbound(vector<int>&arr,int n,int x){
+    int s=0,e=n-1;
+    while(s<=e){
+        int mid=s+(e-s)/2;
+        if(arr[mid]>=x){
+            e=mid-1;
         }
-
+        else{
+            s=mid+1;
+        }
     }
-
-    return rowIndex;
-
- 
-
+    return s;
+}
+int rowWithMax1s(vector<vector<int>> &matrix, int n, int m) {
+    int index=-1,count=0;
+    for(int i=0;i<n;i++){
+        int cnt=m-upperbound(matrix[i],m,1);
+        if(cnt>count){
+            count=cnt;
+            index=i;
+        }
+    }
+    return index;
 }
