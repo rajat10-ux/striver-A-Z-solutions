8c8d054402fc267899ab28e2e0a2a27be57248c0
Rajat Kumar <103815726+rajat10-ux@users.noreply.github.com>
2024-04-04 11:49:59 +0530

    Create nextsmaller.cpp

---
+++ stacks/nextsmaller.cpp
@@ -0,0 +1,15 @@
+    vector<int> help_classmate(vector<int> arr, int n) 
+    { 
+        vector<int>ans;
+        stack<int>st;
+        st.push(-1);
+        for(int i=n-1;i>=0;i--){
+          while(st.top()>=arr[i]){
+              st.pop();
+          }
+            ans.push_back(st.top());
+            st.push(arr[i]);
+        }
+        reverse(ans.begin(),ans.end());
+        return  ans;
+    } 
