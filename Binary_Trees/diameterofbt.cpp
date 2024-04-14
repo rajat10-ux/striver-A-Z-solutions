b7a54446b819e2c1725dd8d9e4b576c3f7401913
Rajat Kumar <103815726+rajat10-ux@users.noreply.github.com>
2024-04-14 14:56:52 +0530

    Update diameterofbt.cpp

--- Binary_Trees/diameterofbt.cpp
+++ Binary_Trees/diameterofbt.cpp
@@ -9,3 +9,26 @@ int heightofbt(TreeNode*root){
         int op3=heightofbt(root->left)+heightofbt(root->right);
         return max(op1,max(op2,op3));
     }
+// second approach
+pair<int,int>diameterfast(TreeNode<int>*root){
+    if(root==NULL){
+        pair<int,int>p=make_pair(0,0);
+        return p;
+    }
+    pair<int,int>left=diameterfast(root->left);
+    pair<int,int>right=diameterfast(root->right);
+    int op1=left.first;
+    int op2=right.first;
+    int op3=left.second+right.second;
+    pair<int,int>ans;
+    ans.first=max(op1,max(op2,op3));
+    ans.second=max(left.second,right.second)+1;
+    return ans;
+}
+int diameterOfBinaryTree(TreeNode<int> *root){
+if(root==NULL){
+    return 0;
+}
+pair<int,int>res=diameterfast(root);
+return res.first;
+}
